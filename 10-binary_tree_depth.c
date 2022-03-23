d577554d4f59dcc3ca5b544d9bcf2fc20e38ecf2
PabloOsorix <3387@holbertonschool.com>
2022-03-23 15:39:51 -0500

    :heavy_plus_sign: function that measures the depth of a node in a binary tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * tree_height - Measures the depth of a binary tree.
+ * @tree: pointer to bainary tree to measure.
+ * Return: depth of the tree, 0 on failure.
+ */
+size_t tree_depth(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (tree_depth(tree->parent) + 1);
+}
+/**
+ * binary_tree_depth - Measure the depth of a node
+ * @tree: pointer to the binary tree.
+ * Return: depth of binary tree, NULL in failure.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+
+	if (!tree)
+		return (0);
+	return (tree_depth(tree) - 1);
+}
