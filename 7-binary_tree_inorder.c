5687c8046cdfd86f5ef7a108102212df550b142d
PabloOsorix <3387@holbertonschool.com>
2022-03-23 11:35:24 -0500

    :pencil2: fix betty

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-//BT = binary tree.
 /**
  * binary_tree_inorder - Function that goes through
  * a binary tree using in-order traversal.
@@ -9,10 +8,10 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (!tree || !func)
-        return;
-    
-    binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    binary_tree_inorder(tree->right, func);
+	if (!tree || !func)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
