6aa5c5d09096ef4f89f9d390c96b6087d38a6b3b
PabloOsorix <3387@holbertonschool.com>
2022-03-23 11:23:15 -0500

    :recycle: Refact binary_tree_preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,17 +11,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 	func(tree->n);
-
-	if (tree->left == NULL)
-		return;
-
-	func(tree->left->n);
-	binary_tree_preorder(tree->left->left, func);
-	binary_tree_preorder(tree->left->right, func);
-
-	if (tree->right == NULL)
-		return;
-	func(tree->right->n);
-	binary_tree_preorder(tree->right->left, func);
-	binary_tree_preorder(tree->right->right, func);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
