f113f83aa82c3dadefb18cb3541fdef38b26d286
PabloOsorix <3387@holbertonschool.com>
2022-03-23 19:51:47 -0500

    :heavy_plus_sign: function that measures the size of a binary tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - function that measures the size of a
+ * binary tree.
+ * @tree: pointer to the binary tree
+ * Return: measures size of binary tree.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (!tree)
+		return (0);
+
+	size = binary_tree_size(tree->left) + binary_tree_size(tree->right);
+	return (size + 1);
+}
