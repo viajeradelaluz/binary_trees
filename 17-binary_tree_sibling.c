eea56d0d8a428d39cdcaafd27f1e3355ee04078b
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-03-24 15:26:24 -0500

    feat: finds the sibling of a node

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Finds the sibling of a node.
+ * @node: points to the node to find the sibling.
+ * Return: the sibling, otherwise NULL.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
